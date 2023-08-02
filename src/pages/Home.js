e6893ee2228235fb46b432a9065648b778f85bb2
Heba <hebahiary+github@gmail.com>
2023-08-02 16:26:57 -0700

    updates

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,8 +8,14 @@ function Home() {
       <img src="/img/Heba.jpg" className="profilepic" />
       <h1>Heba Alhyari</h1>
       <div className="icons">
-        <a href="">
-          <img src="" />
+        <a target="_blank" href="https://github.com/hebahiari">
+          <img src="https://cdn1.iconfinder.com/data/icons/logotypes/32/github-64.png" />
+        </a>
+        <a target="_blank" href="https://www.linkedin.com/in/hebadev/">
+          <img src="https://cdn2.iconfinder.com/data/icons/social-media-2285/512/1_Linkedin_unofficial_colored_svg-512.png" />
+        </a>
+        <a target="_blank" href="https://www.instagram.com/capturingflow/">
+          <img src="https://cdn2.iconfinder.com/data/icons/social-media-2285/512/1_Instagram_colored_svg_1-512.png" />
         </a>
       </div>
       <p>
