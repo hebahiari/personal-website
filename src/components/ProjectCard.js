5785f602ad91b79531b851598c4baa9e96d18761
Heba <hebahiary+github@gmail.com>
2023-08-07 11:36:45 -0700

    added loading screen

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -11,25 +11,28 @@ function ProjectCard({ project }) {
       </a>
 
       <div className="description">
-        <div className="title">
-          <div className="name">
-            <h3>{name}</h3>
-            <p>{type ? type : "type"}</p>
-          </div>
+        <div className="info">
+          {" "}
+          <div className="title">
+            <div className="name">
+              <h3>{name}</h3>
+              <p>{type ? type : "type"}</p>
+            </div>
 
-          <div className="icons">
-            {links[1] ? (
-              <a href={links[1]}>
-                <img src="https://cdn4.iconfinder.com/data/icons/materia-social-free/24/038_030_share_link_friends_send_android_material-64.png" />
+            <div className="icons">
+              {links[1] ? (
+                <a href={links[1]}>
+                  <img src="https://cdn4.iconfinder.com/data/icons/materia-social-free/24/038_030_share_link_friends_send_android_material-64.png" />
+                </a>
+              ) : null}
+              <a href={links[0]}>
+                <img src="https://cdn1.iconfinder.com/data/icons/logotypes/32/github-64.png" />
               </a>
-            ) : null}
-            <a href={links[0]}>
-              <img src="https://cdn1.iconfinder.com/data/icons/logotypes/32/github-64.png" />
-            </a>
+            </div>
           </div>
+          <p>{desc}</p>
         </div>
 
-        <p>{desc}</p>
         <div className="tags">
           {tags.map((tag) => (
             <div className="tag">{tag}</div>
