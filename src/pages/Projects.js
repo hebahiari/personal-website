122825645eb0043637a41c6d93f50cae3688cd79
Heba <hebahiary+github@gmail.com>
2023-08-08 09:42:05 -0700

    style updates

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -7,8 +7,8 @@ let projects = [
     name: "Petsgram",
     type: "(Social media website)",
 
-    desc: "A MERN full-stack social media website with a mobile-first, responsive design. It has a React frontend, an Express and NodeJS backend, and uses Postgres as its database.",
-    tags: ["JavaScript", "React", "Express", "NodeJS", "Postgres"],
+    desc: "A MERN full-stack social media website with a mobile-first, responsive design. It has a React frontend, an Express and Node.js backend, and uses Postgres as its database.",
+    tags: ["JavaScript", "React", "Express", "Node.js", "Postgres"],
     image: "/img/portfolio/7.PNG",
     links: [
       "https://github.com/hebahiari/petsgram",
@@ -20,7 +20,7 @@ let projects = [
     name: "TechWearCo",
     type: "(E-commerce website)",
     desc: "An e-commerce website built to sell products, with a React frontend that utilizes react router and redux for smooth navigation and user experience. The backend of the site is developed using Strapi, which also serves as the database management system for the site. To facilitate online transactions, the site uses Stripe as its payment portal.",
-    tags: ["JavaScript", "React", "Express", "NodeJS", "Postgres"],
+    tags: ["JavaScript", "React", "Redux", "SCSS", "Strapi", "Stripe"],
     image: "/img/portfolio/5.jpg",
     links: [
       "https://github.com/hebahiari/ecommerce",
@@ -44,7 +44,7 @@ let projects = [
     name: "Booked.",
     type: "(Scheduling website)",
     desc: "This is a full stack restaurant reservation system built with JavaScript/React for the frontend, and Node/Express with Knex for the backend. The website is designed to be responsive, with a user-friendly UI styled using CSS and Bootstrap.   ",
-    tags: ["JavaScript", "React", "NodeJS", "Express", "Knex"],
+    tags: ["JavaScript", "React", "Node.js", "Express", "Knex"],
     image: "/img/portfolio/1.jpg",
     links: [
       "https://github.com/hebahiari/restaurant-reservation",
@@ -55,7 +55,7 @@ let projects = [
   {
     name: "GrubDash",
     desc: "",
-    tags: ["HTML", "CSS", "React", "Express", "NodeJS", "Postgres"],
+    tags: ["Javascript", "Express", "Node.js", "Postgres"],
     image: "/img/portfolio/2.jpg",
     links: ["https://github.com/hebahiari/GrubDash"],
   },
@@ -63,7 +63,7 @@ let projects = [
   {
     name: "WeLoveMovies",
     desc: "",
-    tags: ["HTML", "CSS", "React", "Express", "NodeJS", "Postgres"],
+    tags: ["JavaScript", "React", "Express", "Node.js", "Postgres", "Knex"],
     image: "/img/portfolio/3.jpg",
     links: ["https://github.com/hebahiari/we-love-movies"],
   },
@@ -71,7 +71,7 @@ let projects = [
   {
     name: "Flashcard-o-matic",
     desc: "",
-    tags: ["HTML", "CSS", "React", "Express", "NodeJS", "Postgres"],
+    tags: ["JavaScript", "React", "Bootstrap"],
     image: "/img/portfolio/4.jpg",
     links: ["https://github.com/hebahiari/flashcard-app"],
   },
